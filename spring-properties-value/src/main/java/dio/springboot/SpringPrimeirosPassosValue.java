f393558ff8abeed016d67c82984cb7bd9f45bc6d
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-02-17 12:00:00 -0300

    Propeties Value

---
+++ spring-properties-value/src/main/java/dio/springboot/SpringPrimeirosPassosValue.java
@@ -0,0 +1,14 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrimeirosPassosValue {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrimeirosPassosValue.class, args);
+	}
+	
+
+}
