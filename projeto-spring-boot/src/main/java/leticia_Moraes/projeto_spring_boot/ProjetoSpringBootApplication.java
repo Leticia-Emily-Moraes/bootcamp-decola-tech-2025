ba0c6f49cb20eb1e2f873cf283bcd3273638da94
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-02-26 12:00:00 -0300

    feat: add main class ProjetoSpringBootApplication with initial Spring Boot configuration

---
+++ projeto-spring-boot/src/main/java/leticia_Moraes/projeto_spring_boot/ProjetoSpringBootApplication.java
@@ -0,0 +1,14 @@
+package leticia_Moraes.projeto_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
